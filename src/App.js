c8451eb35592cc5b14bdc130ab3bc47f472223e0
Danna Bohnhoff <dannab101@gmail.com>
2023-09-17 17:24:01 -0400

    message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { useState } from 'react';
+import ColorBlock from './ColorBlock';  // Updated correct casing
+import ColorForm from './ColorForm';  // Updated correct casing
+
+function App(){
+    let [colors, setColors] = useState([
+        'violet', 'blue',
+        'lightblue', 'green',
+        'greenyellow', 'yellow',
+        'orange', 'red'
+    ])
+    
+    let colorMap = colors.map((color, i) => {
+        return (
+            <ColorBlock color={color} />
+        )
+    })
+
+    return (
+        <div className="App">
+            {colorMap}
+            <ColorForm />
+        </div>
+    )
 }
+
+
 
 export default App;
